a611b29f4ac86592e2c2e8d118ce96fab0a685e6
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-10-04 15:56:42 +0530

    Update EmailValidator.java

--- com/capgemini/EmailValidator.java
+++ com/capgemini/EmailValidator.java
@@ -9,7 +9,7 @@ public class EmailValidator {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email Validator");
 		Pattern p= Pattern.compile("(^[a-zA-Z0-9]+)");
-		Matcher m=p.matcher("krishna");
+		Matcher m=p.matcher("kevin");
 		boolean b=m.matches();
 		if(b) {
 			System.out.println("Matches");
